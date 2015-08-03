5b1e4fc5b7dbe36b024e07f0e3d7a9f3baa86838
Rajiv Nair <rajiv.nair93@gmail.com>
2015-08-03 16:06:19 -0500

    updated setup.py (fixed bug in entry_points)

--- setup.py
+++ setup.py
@@ -22,22 +22,15 @@
 """
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setup(
     name='UIUCEnterpriseBot',
-    version='0.1',
+    version='0.2',
 
     description='A webbot for interacting with the Univ. of Illinois '
                 'course registration website.',
-    long_description=long_description,
+    long_description='Long Desc',
 
     # The project's main homepage.
     url='https://github.com/rsnair2/UIUCEnterpriseBot',
@@ -57,8 +50,8 @@ setup(
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Students of Univ. of Illinois',
-        'Topic :: Web Automation',
+        'Intended Audience :: Education',
+        'Topic :: Internet :: WWW/HTTP',
 
         'License :: OSI Approved :: MIT License',
 
@@ -80,31 +73,9 @@ setup(
     install_requires=['requests',
                       'keyring'],
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'poll-course=src.main.main',
+            'webby=src.main:main',
         ],
     },
 )

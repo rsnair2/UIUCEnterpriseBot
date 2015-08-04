7d530276fcd3ff2d01dd9a6d360a9715da297da3
Rajiv Nair <rajiv.nair93@gmail.com>
2015-08-03 22:00:30 -0500

    updated setup.py, __init__.py to make it easier to use import directive to import the modules in UIUCEnterpriseBot package

--- setup.py
+++ setup.py
@@ -1,81 +1,58 @@
-"""
-    UIUC Enterprise Webbot: a webbot to monitor the availability of a class
-    through the UIUC Enterprise system.
+#!/usr/bin/env python
 
-    This file is part of UIUC Enterprise Webbot.
+# UIUCEnterpriseBot : a webbot that interacts with the
+# University of Illinois course registration system
+# Copyright (C) 2015  Rajiv Nair
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    UIUC Enterprise Webbot is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+from codecs import open
 
-    UIUC Enterprise Webbot is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-    You should have received a copy of the GNU General Public License
-    along with UIUC Enterprise Webbot. If not, see
-    <http://www.gnu.org/licenses/>.
-
-    author: Rajiv Nair (rsnair.com)
-"""
-
-from setuptools import setup, find_packages
-
+with open('README.rst', 'r', 'utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='UIUCEnterpriseBot',
-    version='0.2',
-
-    description='A webbot for interacting with the Univ. of Illinois '
-                'course registration website.',
-    long_description='Long Desc',
-
-    # The project's main homepage.
+    version='0.3.0',
+    description='Webbot for interacting with Univ. of Illinois course '
+                'registration system.',
+    long_description=long_description,
     url='https://github.com/rsnair2/UIUCEnterpriseBot',
-
-    # Author details
     author='Rajiv Nair',
     author_email='rsnair2@me.com',
-
     license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Education',
         'Topic :: Internet :: WWW/HTTP',
-
         'License :: OSI Approved :: MIT License',
-
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
-
     keywords='uiuc webbot automation',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=['UIUCEnterpriseBot'],
     install_requires=['requests',
                       'keyring'],
-
     entry_points={
         'console_scripts': [
-            'webby=src.main:main',
+            'uiuc-enterprise-bot=UIUCEnterpriseBot.main:main',
         ],
     },
 )
